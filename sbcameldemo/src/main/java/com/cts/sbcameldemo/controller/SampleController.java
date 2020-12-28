42ee3319ebf31a97abf16c62a079929ffcd0bc28
Saibal Ghosh <428145@cognizant.com>
2020-12-28 00:21:10 +0000

    NA

--- sbcameldemo/src/main/java/com/cts/sbcameldemo/controller/SampleController.java
+++ sbcameldemo/src/main/java/com/cts/sbcameldemo/controller/SampleController.java
@@ -1,14 +1,17 @@
 package com.cts.sbcameldemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	@GetMapping(value = "/hello/{name}")
 	public String sayHello(@PathVariable("name")String name) {
+		logger.info("The input is : "+name);
 		return "Hello "+name.toUpperCase();
 	}
 
