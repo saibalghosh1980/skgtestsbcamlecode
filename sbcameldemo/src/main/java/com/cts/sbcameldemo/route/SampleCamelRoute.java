39e11afbea0fc6b11ac4857089c8f82ffb8dedcd
Saibal Ghosh <428145@cognizant.com>
2020-12-28 00:53:36 +0000

    NA

---
+++ sbcameldemo/src/main/java/com/cts/sbcameldemo/route/SampleCamelRoute.java
@@ -0,0 +1,24 @@
+package com.cts.sbcameldemo.route;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component("springManagedSampleCamelRoute")
+public class SampleCamelRoute extends RouteBuilder {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		from("timer://foo?repeatCount=1").routeId("id_SampleRouteRoute")
+				.log(LoggingLevel.INFO, logger, "Route started .....................................")
+				.toD("https://reqres.in/api/users" +
+					    "?httpMethod=GET")
+				.to("log:"+logger.getName()+"?level=INFO&showAll=true&multiline=true&showStreams=true");
+				
+
+	}
+}
