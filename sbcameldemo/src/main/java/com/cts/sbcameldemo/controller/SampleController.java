997ffcc15ecc939c8f2a8573da4ec33c26b4090c
Saibal Ghosh <428145@cognizant.com>
2020-12-29 22:58:07 +0000

    Update SampleController.java

--- sbcameldemo/src/main/java/com/cts/sbcameldemo/controller/SampleController.java
+++ sbcameldemo/src/main/java/com/cts/sbcameldemo/controller/SampleController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cts.sbcameldemo.config.YamlConfigProperties;
 
 @RestController
+@RefreshScope
 public class SampleController {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
