28076eb7010f3bdf15f08909dae8fe81ab4462fb
Saibal Ghosh <428145@cognizant.com>
2020-12-29 23:21:36 +0000

    Update YamlConfigProperties.java

--- sbcameldemo/src/main/java/com/cts/sbcameldemo/config/YamlConfigProperties.java
+++ sbcameldemo/src/main/java/com/cts/sbcameldemo/config/YamlConfigProperties.java
@@ -3,12 +3,14 @@ package com.cts.sbcameldemo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration("springManagedYamlConfigProperties")
 @EnableConfigurationProperties
 @ConfigurationProperties
+@RefreshScope
 public class YamlConfigProperties {
 	
 	
